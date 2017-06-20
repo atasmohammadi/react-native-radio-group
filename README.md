9c7245e12f4a103a5f98a169d470e4a65eddd2a9
Ata S.Mohammadi <ata.smohammadi@opencampus.net>
2017-06-20 13:51:26 +0200

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # react-native-radio-group
-react-native-radio-group component
+React native radio group component
+
+### Install :
+```sh
+npm install --save react-native-radio-group
+```
+### Example usage :
+```javascript
+import RadioGroup from 'react-native-radio-group'
+
+<RadioGroup
+              callback={(selected) => { console.log(selected) }}
+              iconColor={"#00a2dd"}
+              iconSize={30}
+              checkedIcon="ios-radio-button-on-outline"
+              uncheckedIcon="ios-radio-button-off-outline"
+              radios={[
+                {
+                  label: "first", // label for checkbox item
+                  value: 1, // selected value for item, if selected, what value should be sent?
+                  selected: true // if the item is selected by default or not.
+                },
+                {
+                  label: "second",
+                  value: 2
+                },
+              ]}
+              labelStyle={{
+                color: '#333'
+              }}
+              rowStyle={{
+                flexDirection: 'row'
+              }}
+              rowDirection={"column"}
+            />
+```
+
+![react-native-radio-group](https://raw.githubusercontent.com/ataomega/react-native-checkbox-group/master/screenshot.png)
+
+### Props:
+| Prop | Type | Description |
+| ------ | ------ | ------ |
+| callback | Function | whenever user selects or changes selections it will be called. |
+| iconColor | String | icon color for checked/unchecked icons and search icon also border color of search bar |
+| iconSize | Integer | icon size for checked/unchecked icons |
+| checkedIcon | String | selected/checked icon name (react-native-vector-icons/Ionicon) |
+| uncheckedIcon | String | unselected/unchecked icon name (react-native-vector-icons/Ionicon) |
+| radios | Object | checkboxes. see the example. |
+| labelStyle | Object | styles for checkbox label |
+| rowStyle | Object | styles for each checkbox |
+| rowDirection | String | column or row. how to list checkboxes |
+
+### Author:
+Ata S.Mohammadi.
+ataomega@gmail.com
---
+++ radioGroup.js
@@ -0,0 +1,92 @@
+/**
+ * Radio group
+ * ataomega@gmail.com
+ * www.atasmohammadi.net
+ * version 1.0
+ */
+import React, {Component, PropTypes} from "react";
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+var { width, height } = Dimensions.get('window');
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default class RadioGroup extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pageWidth: Dimensions.get('window').width,
+      pageHeight: Dimensions.get('window').height,
+      selected: []
+    };
+  }
+
+  getNewDimensions(event){
+        var pageHeight = event.nativeEvent.layout.height
+        var pageWidth = event.nativeEvent.layout.width
+        this.setState({
+            pageHeight, pageWidth
+        })
+    }
+
+  _onSelect = (item) => {
+    var selected = this.state.selected
+    if(selected.indexOf(item) == -1){
+      this.setState({
+        selected: [item]
+      })
+    } else {
+      this.setState({
+        selected: []
+      })
+    }
+    this.props.callback(selected)
+  }
+
+  _isSelected = (item) => {
+    const selected = this.state.selected
+    if(selected.indexOf(item) == -1){
+      return false
+    }
+    return true
+  }
+
+  render(){
+    const { radios, iconColor, iconSize, labelStyle, checkedIcon, uncheckedIcon, rowStyle, rowDirection } = this.props;
+
+    return(
+      <View
+        onLayout={(evt)=>{this.getNewDimensions(evt)}}
+        style={{
+          flex: 1,
+          flexDirection: rowDirection,
+          padding: 5
+        }}
+      >
+        {radios.map((checkbox, index)=>{
+          if(checkbox.selected){
+            this._onSelect(checkbox.value)
+          }
+          return(
+            <TouchableOpacity
+              key={index}
+              style={rowStyle}
+              onPress={()=>{
+                this._onSelect(checkbox.value)
+              }}
+            >
+              {this._isSelected(checkbox.value) ?
+                <Icon name={checkedIcon} color={iconColor} size={iconSize}/>
+                : <Icon name={uncheckedIcon} color={iconColor} size={iconSize}/>
+              }
+              <Text style={labelStyle}>{checkbox.label}</Text>
+            </TouchableOpacity>
+          )
+        })}
+      </View>
+    );
+  }
+}
